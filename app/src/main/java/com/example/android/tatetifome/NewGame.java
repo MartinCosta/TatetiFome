38fe65bcac1ab5050aebd67be41223d526c16107
milton <reinoso.vm@gmail.com>
2017-06-02 23:42:50 -0300

    Added an array to substitute various variables. Deleted a lot of unused variables. Fixed bug that makes turns advance when pressing an occupied box. Fixed "sp" instead of "dp" problem in menu xml (activity_about.xml and activity_main.xml)

--- app/src/main/java/com/example/android/tatetifome/NewGame.java
+++ app/src/main/java/com/example/android/tatetifome/NewGame.java
@@ -5,43 +5,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import static android.os.Build.VERSION_CODES.M;
+
 import static com.example.android.tatetifome.R.drawable.o;
+
 /**
  * Created by Martin on 2/6/2017.
  */
 public class NewGame extends AppCompatActivity {
 
-    boolean isLockedB7;         /* booleans to unlocked or locked Boxes*/
-    boolean isLockedB8;
-    boolean isLockedB9;
-    boolean isLockedB4;
-    boolean isLockedB5;
-    boolean isLockedB6;
-    boolean isLockedB1;
-    boolean isLockedB2;
-    boolean isLockedB3;
     String turnOfX = "X";
     String turnOfO = "O";
     int turnCounter = 1;
-    boolean xBox1_check = false;    /* booleans variables to check player winner and display "X" or "O"*/
-    boolean xBox2_check = false;
-    boolean xBox3_check = false;
-    boolean xBox4_check = false;
-    boolean xBox5_check = false;
-    boolean xBox6_check = false;
-    boolean xBox7_check = false;
-    boolean xBox8_check = false;
-    boolean xBox9_check = false;
-    boolean oBox1_check = false;
-    boolean oBox2_check = false;
-    boolean oBox3_check = false;
-    boolean oBox4_check = false;
-    boolean oBox5_check = false;
-    boolean oBox6_check = false;
-    boolean oBox7_check = false;
-    boolean oBox8_check = false;
-    boolean oBox9_check = false;
+    /** Building an Array that can store the status of all 9 boxes in the game. E.g boxArray [7] = 1 ==> "the box in the upper left corner is now an X".
+     * 0 means it hasn´t been marked (pressed) yet neither by X or 0.
+     * 1 means it´s been marked with an X.
+     * -1 means it´s been marked with an O.
+      */
+    int[] boxArray = new int[10];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,130 +29,163 @@ public class NewGame extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    /** Mark method, main method for marking and checking boxes
+     *
+     * @param view
+     */
     public void mark(View view) {
 
         switch (view.getId()) {
             case R.id.box1:
                 ImageButton setMarkInBox1 = (ImageButton) findViewById(R.id.box1);
-                if (turnCounter % 2 == 1 && isLockedB1 == false) {
-                    setMarkInBox1.setImageResource(R.drawable.x);
-                    xBox1_check = true;
-                } else if (turnCounter % 2 != 1 && isLockedB1 == false) {
-                    setMarkInBox1.setImageResource(o);
-                    oBox1_check = true;
+
+                if (boxArray[1]!=0){                        //Stops the method if not empty
+                    displayMessageWin("Not an empty box");
+                    break;
                 }
-                isLockedB1 = true;
+                if (boxArray[1] == 0 && turnCounter % 2 == 1) {
+                    setMarkInBox1.setImageResource(R.drawable.x);
+                    boxArray[1]=1;
+                } else if (boxArray[1] == 0 && turnCounter % 2 != 1) {
+                    setMarkInBox1.setImageResource(o);
+                    boxArray [1] = -1;
+                }
                 turnCounter++;
                 checkForPlayer();
                 checkWin();
                 break;
             case R.id.box2:
                 ImageButton setMarkInBox2 = (ImageButton) findViewById(R.id.box2);
-                if (turnCounter % 2 == 1 && isLockedB2 == false) {
-                    setMarkInBox2.setImageResource(R.drawable.x);
-                    xBox2_check = true;
-                } else if (turnCounter % 2 != 1 && isLockedB2 == false) {
-                    setMarkInBox2.setImageResource(o);
-                    oBox2_check = true;
+                if (boxArray[2]!=0){                        //Stops the method if not empty
+                    displayMessageWin("Not an empty box");
+                    break;
                 }
-                isLockedB2 = true;
+                if (boxArray[2] == 0 && turnCounter % 2 == 1) {
+                    setMarkInBox2.setImageResource(R.drawable.x);
+                    boxArray[2]=1;
+                } else if (boxArray[2] == 0 && turnCounter % 2 != 1) {
+                    setMarkInBox2.setImageResource(o);
+                    boxArray [2] = -1;
+                }
                 turnCounter++;
                 checkForPlayer();
                 checkWin();
                 break;
             case R.id.box3:
                 ImageButton setMarkInBox3 = (ImageButton) findViewById(R.id.box3);
-                if (turnCounter % 2 == 1 && isLockedB3 == false) {
-                    setMarkInBox3.setImageResource(R.drawable.x);
-                    xBox3_check = true;
-                } else if (turnCounter % 2 != 1 && isLockedB3 == false) {
-                    setMarkInBox3.setImageResource(o);
-                    oBox3_check = true;
+                if (boxArray[3]!=0){                        //Stops the method if not empty
+                    displayMessageWin("Not an empty box");
+                    break;
                 }
-                isLockedB3 = true;
+                if (boxArray[3] == 0 && turnCounter % 2 == 1) {
+                    setMarkInBox3.setImageResource(R.drawable.x);
+                    boxArray[3]=1;
+                } else if (boxArray[3] == 0 && turnCounter % 2 != 1) {
+                    setMarkInBox3.setImageResource(o);
+                    boxArray [3] = -1;
+                }
                 turnCounter++;
+                checkForPlayer();
                 checkWin();
                 break;
             case R.id.box4:
                 ImageButton setMarkInBox4 = (ImageButton) findViewById(R.id.box4);
-                if (turnCounter % 2 == 1 && isLockedB4 == false) {
-                    setMarkInBox4.setImageResource(R.drawable.x);
-                    xBox4_check = true;
-                } else if (turnCounter % 2 != 1 && isLockedB4 == false) {
-                    setMarkInBox4.setImageResource(o);
-                    oBox4_check = true;
+                if (boxArray[4]!=0){                        //Stops the method if not empty
+                    displayMessageWin("Not an empty box");
+                    break;
                 }
-                isLockedB4 = true;
+                if (boxArray[4] == 0 && turnCounter % 2 == 1) {
+                    setMarkInBox4.setImageResource(R.drawable.x);
+                    boxArray[4]=1;
+                } else if (boxArray[4] == 0 && turnCounter % 2 != 1) {
+                    setMarkInBox4.setImageResource(o);
+                    boxArray [4] = -1;
+                }
                 turnCounter++;
                 checkForPlayer();
                 checkWin();
                 break;
             case R.id.box5:
                 ImageButton setMarkInBox5 = (ImageButton) findViewById(R.id.box5);
-                if (turnCounter % 2 == 1 && isLockedB5 == false) {
-                    setMarkInBox5.setImageResource(R.drawable.x);
-                    xBox5_check = true;
-                } else if (turnCounter % 2 != 1 && isLockedB5 == false) {
-                    setMarkInBox5.setImageResource(o);
-                    oBox5_check = true;
+                if (boxArray[5]!=0){                        //Stops the method if not empty
+                    displayMessageWin("Not an empty box");
+                    break;
                 }
-                isLockedB5 = true;
+                if (boxArray[5] == 0 && turnCounter % 2 == 1) {
+                    setMarkInBox5.setImageResource(R.drawable.x);
+                    boxArray[5]=1;
+                } else if (boxArray[5] == 0 && turnCounter % 2 != 1) {
+                    setMarkInBox5.setImageResource(o);
+                    boxArray[5]=-1;
+                }
                 turnCounter++;
                 checkForPlayer();
                 checkWin();
                 break;
             case R.id.box6:
-                ImageButton setMarkInBox6 = (ImageButton) findViewById(R.id.box6);
-                if (turnCounter % 2 == 1 && isLockedB6 == false) {
-                    setMarkInBox6.setImageResource(R.drawable.x);
-                    xBox6_check = true;
-                } else if (turnCounter % 2 != 1 && isLockedB6 == false) {
-                    setMarkInBox6.setImageResource(o);
-                    oBox6_check = true;
+                if (boxArray[6]!=0){                        //Stops the method if not empty
+                    displayMessageWin("Not an empty box");
+                    break;
                 }
-                isLockedB6 = true;
+                ImageButton setMarkInBox6 = (ImageButton) findViewById(R.id.box6);
+                if (boxArray[6] == 0 && turnCounter % 2 == 1) {
+                    setMarkInBox6.setImageResource(R.drawable.x);
+                    boxArray[6]=1;
+                } else if (boxArray[6] == 0 && turnCounter % 2 != 1) {
+                    setMarkInBox6.setImageResource(o);
+                    boxArray[6]=-1;
+                }
                 turnCounter++;
                 checkForPlayer();
                 checkWin();
                 break;
             case R.id.box7:
-                ImageButton setMarkInBox7 = (ImageButton) findViewById(R.id.box7);
-                if (turnCounter % 2 == 1 && isLockedB7 == false) {
-                    setMarkInBox7.setImageResource(R.drawable.x);
-                    xBox7_check = true;
-                } else if (turnCounter % 2 != 1 && isLockedB7 == false) {
-                    setMarkInBox7.setImageResource(o);
-                    oBox7_check = true;
+                if (boxArray[7]!=0){                        //Stops the method if not empty
+                    displayMessageWin("Not an empty box");
+                    break;
                 }
-                isLockedB7 = true;
+                ImageButton setMarkInBox7 = (ImageButton) findViewById(R.id.box7);
+                if (boxArray[7] == 0 && turnCounter % 2 == 1) {
+                    setMarkInBox7.setImageResource(R.drawable.x);
+                    boxArray[7]=1;
+                } else if (boxArray[7] == 0 && turnCounter % 2 != 1) {
+                    setMarkInBox7.setImageResource(o);
+                    boxArray[7]=-1;
+                }
                 turnCounter++;
                 checkForPlayer();
                 checkWin();
                 break;
             case R.id.box8:
                 ImageButton setMarkInBox8 = (ImageButton) findViewById(R.id.box8);
-                if (turnCounter % 2 == 1 && isLockedB8 == false) {
-                    setMarkInBox8.setImageResource(R.drawable.x);
-                    xBox8_check = true;
-                } else if (turnCounter % 2 != 1 && isLockedB8 == false) {
-                    setMarkInBox8.setImageResource(o);
-                    oBox8_check = true;
+                if (boxArray[8]!=0){                        //Stops the method if not empty
+                    displayMessageWin("Not an empty box");
+                    break;
                 }
-                isLockedB8 = true;
+                if (boxArray[8] == 0 && turnCounter % 2 == 1) {
+                    setMarkInBox8.setImageResource(R.drawable.x);
+                    boxArray[8]=1;
+                } else if (boxArray[8] == 0 && turnCounter % 2 != 1) {
+                    setMarkInBox8.setImageResource(o);
+                    boxArray[8]=-1;
+                }
                 turnCounter++;
                 checkForPlayer();
                 checkWin();
                 break;
             case R.id.box9:
-                ImageButton setMarkInBox9 = (ImageButton) findViewById(R.id.box9);
-                if (turnCounter % 2 == 1 && isLockedB9 == false) {
-                    setMarkInBox9.setImageResource(R.drawable.x);
-                    xBox9_check = true;
-                } else if (turnCounter % 2 != 1 && isLockedB9 == false) {
-                    setMarkInBox9.setImageResource(o);
-                    oBox9_check = true;
+                if (boxArray[9]!=0){                        //Stops the method if not empty
+                    displayMessageWin("Not an empty box");
+                    break;
                 }
-                isLockedB9 = true;
+                ImageButton setMarkInBox9 = (ImageButton) findViewById(R.id.box9);
+                if (boxArray[9] == 0 && turnCounter % 2 == 1) {
+                    setMarkInBox9.setImageResource(R.drawable.x);
+                    boxArray[9]=1;
+                } else if (boxArray[5] == 0 && turnCounter % 2 != 1) {
+                    setMarkInBox9.setImageResource(o);
+                    boxArray[9]=-1;
+                }
                 turnCounter++;
                 checkForPlayer();
                 checkWin();
@@ -184,82 +197,82 @@ public class NewGame extends AppCompatActivity {
         String winX = "PLayer X is the winner!";
         String winO = "Player O is the winner!";
 
-        if ((xBox1_check && xBox2_check && xBox3_check) == true) {
+        if (boxArray[1]==1 && boxArray[2]==1 && boxArray[3]==1) {
 
-            View verticalView1 = findViewById(R.id.winHorizontal03);
-            verticalView1.setVisibility(View.VISIBLE);
+//            View verticalView1 = findViewById(R.id.winHorizontal03);
+//            verticalView1.setVisibility(View.VISIBLE);
             displayMessageWin(winX);
 
-        } else if ((oBox1_check && oBox2_check && oBox3_check) == true) {
+        } else if (boxArray[1]==-1 && boxArray[2]==-1 && boxArray[3]==-1) {
 
-            View verticalView1 = findViewById(R.id.winHorizontal03);
-            verticalView1.setVisibility(View.VISIBLE);
+//            View verticalView1 = findViewById(R.id.winHorizontal03);
+//            verticalView1.setVisibility(View.VISIBLE);
             displayMessageWin(winO);
 
-        } else if ((xBox1_check && xBox4_check && xBox7_check) == true) {
+        } else if (boxArray[1]==1 && boxArray[4]==1 && boxArray[7]==1) {
 
             displayMessageWin(winX);
 
-        } else if ((oBox1_check && oBox4_check && oBox7_check) == true) {
-
-            displayMessageWin(winO);
-
-        } else if ((xBox4_check && xBox5_check && xBox6_check) == true) {
-
-            View verticalView1 = findViewById(R.id.winHorizontal02);
-            verticalView1.setVisibility(View.VISIBLE);
-            displayMessageWin(winX);
-        } else if ((oBox4_check && oBox5_check && oBox6_check) == true) {
-
-            View verticalView1 = findViewById(R.id.winHorizontal02);
-            verticalView1.setVisibility(View.VISIBLE);
-            displayMessageWin(winO);
-
-        } else if ((xBox7_check && xBox8_check && oBox9_check) == true) {
-
-            View verticalView1 = findViewById(R.id.winHorizontal01);
-            verticalView1.setVisibility(View.VISIBLE);
-            displayMessageWin(winX);
-
-        } else if ((oBox7_check && oBox8_check && oBox9_check) == true) {
-
-            View verticalView1 = findViewById(R.id.winHorizontal01);
-            verticalView1.setVisibility(View.VISIBLE);
-            displayMessageWin(winO);
-
-        } else if ((xBox2_check && xBox5_check && xBox8_check) == true) {
-
-            View verticalView1 = findViewById(R.id.winVertical01);
-            verticalView1.setVisibility(View.VISIBLE);
-            displayMessageWin(winX);
-
-        } else if ((oBox2_check && oBox5_check && oBox8_check) == true) {
-
-            View verticalView1 = findViewById(R.id.winVertical01);
-            verticalView1.setVisibility(View.VISIBLE);
-            displayMessageWin(winO);
-
-        } else if ((xBox3_check && xBox6_check && xBox9_check) == true) {
-
-            displayMessageWin(winX);
-
-        } else if ((oBox3_check && oBox6_check && oBox9_check) == true) {
+        } else if (boxArray[1]==-1 && boxArray[4]==-1 && boxArray[7]==-1) {
 
             displayMessageWin(winO);
 
-        } else if ((xBox1_check && xBox5_check && xBox9_check) == true) {
+        } else if (boxArray[4]==1 && boxArray[5]==1 && boxArray[6]==1) {
+
+//            View verticalView1 = findViewById(R.id.winHorizontal02);
+//            verticalView1.setVisibility(View.VISIBLE);
+            displayMessageWin(winX);
+        } else if (boxArray[4]==-1 && boxArray[5]==-1 && boxArray[6]==-1) {
+
+//            View verticalView1 = findViewById(R.id.winHorizontal02);
+//            verticalView1.setVisibility(View.VISIBLE);
+            displayMessageWin(winO);
+
+        } else if (boxArray[7]==1 && boxArray[8]==1 && boxArray[9]==1) {
+
+//            View verticalView1 = findViewById(R.id.winHorizontal01);
+//            verticalView1.setVisibility(View.VISIBLE);
+            displayMessageWin(winX);
+
+        } else if (boxArray[7]==-1 && boxArray[8]==-1 && boxArray[9]==-1) {
+
+//            View verticalView1 = findViewById(R.id.winHorizontal01);
+//            verticalView1.setVisibility(View.VISIBLE);
+            displayMessageWin(winO);
+
+        } else if (boxArray[2]==1 && boxArray[5]==1 && boxArray[8]==1){
+
+//            View verticalView1 = findViewById(R.id.winVertical01);
+//            verticalView1.setVisibility(View.VISIBLE);
+            displayMessageWin(winX);
+
+        } else if (boxArray[2]==-1 && boxArray[5]==-1 && boxArray[8]==-1) {
+
+//            View verticalView1 = findViewById(R.id.winVertical01);
+//            verticalView1.setVisibility(View.VISIBLE);
+            displayMessageWin(winO);
+
+        } else if (boxArray[3]==1 && boxArray[6]==1 && boxArray[9]==1) {
 
             displayMessageWin(winX);
 
-        } else if ((oBox1_check && oBox5_check && oBox9_check) == true) {
+        } else if (boxArray[3]==-1 && boxArray[6]==-1 && boxArray[9]==-1) {
 
             displayMessageWin(winO);
 
-        } else if ((xBox3_check && xBox5_check && xBox7_check) == true) {
+        } else if (boxArray[1]==1 && boxArray[5]==1 && boxArray[9]==1) {
 
             displayMessageWin(winX);
 
-        } else if ((oBox3_check && oBox5_check && oBox7_check) == true) {
+        } else if (boxArray[1]==-1 && boxArray[5]==-1 && boxArray[9]==-1) {
+
+            displayMessageWin(winO);
+
+        } else if (boxArray[3]==1 && boxArray[5]==1 && boxArray[7]==1) {
+
+            displayMessageWin(winX);
+
+        } else if (boxArray[3]==-1 && boxArray[5]==-1 && boxArray[7]==-1) {
 
             displayMessageWin(winO);
         }
